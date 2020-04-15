badc1ba0f86fb39a000db8514d49791747977923
Hasanuzzaman <hasanuzzamanbe@gmail.com>
2020-04-15 11:16:31 +0600

    options page added

--- back.js
+++ back.js
@@ -10,20 +10,26 @@ document.addEventListener("DOMContentLoaded", function () {
     // }
 });
 
-window.onload = function () {
-    var $loader = $('#loader');
-    $loader.show();
 
-    chrome.tabs.query({
-        "currentWindow": true,  //Filters tabs in current window
-        "status": "complete",   //The Page is completely loaded
-        "active": true,         // The tab or web page is browsed at this state,
-        "windowType": "normal"  // Filters normal web pages, eliminates g-talk notifications etc
-    }, function (tabs) {        //It returns an array
-        for (tab in tabs) {
-            $('#url').val(tabs[tab].url);
-            $('#title').val(tabs[tab].title);
-            $loader.hide();
-        }
-    });
-};
+/* 
+*
+* will work later
+*/
+ 
+// window.onload = function () {
+//     var $loader = $('#loader');
+//     $loader.show();
+
+//     chrome.tabs.query({
+//         "currentWindow": true,  //Filters tabs in current window
+//         "status": "complete",   //The Page is completely loaded
+//         "active": true,         // The tab or web page is browsed at this state,
+//         "windowType": "normal"  // Filters normal web pages, eliminates g-talk notifications etc
+//     }, function (tabs) {        //It returns an array
+//         for (tab in tabs) {
+//             $('#url').val(tabs[tab].url);
+//             $('#title').val(tabs[tab].title);
+//             $loader.hide();
+//         }
+//     });
+// };
