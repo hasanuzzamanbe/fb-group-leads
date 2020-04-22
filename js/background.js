e26509f4a86df16280965d20f9aa81cda246a512
Hasanuzzaman <hasanuzzamanbe@gmail.com>
2020-04-22 12:17:04 +0600

    context menu added, ui changed

---
+++ js/background.js
@@ -0,0 +1,13 @@
+var showForPages = ["*://docs.google.com/forms/*"];
+
+chrome.contextMenus.create({
+    "title": "FB Group Leads- set API",
+    "id": "fbglgetapi",
+    "documentUrlPatterns": showForPages
+});
+
+chrome.contextMenus.onClicked.addListener((clickData) => {
+   
+})
+
+
--- js/setup.js
+++ js/setup.js
@@ -1,11 +1,10 @@
 var apiPath = window.location.href;
 
 var infoHtml = '<div style="padding: 12px;background: aliceblue;border: 1px solid #ddd7d7;border-radius: 12px;"><p>API Url:  </p><p>' + apiPath.replace('/viewform', '/formResponse?') + '</p>';
-infoHtml += '<i style="color: green;font-size: 12px;margin-top:7px;">Collect the fields value. Ex: <span style="color:darkred;">"entry.XXXXXXX" </span></i>'
+infoHtml += '<br/><i style="color: green;font-size: 12px;margin-top:7px;">copy the line bellow and put on fields. Ex: <span style="color:darkred;">"entry.1007858859,entry.893612991,entry.315525019" </span></i><br/>'
 if ((document.domain + '/forms') == 'docs.google.com/forms' && $("input[name^='entry']").get(0)) {
     var elements = $("[name^='entry.']");
     for (let i = 0; i < elements.length; i++) {
-        // infoHtml += '<h4>' + 'field' + (i + 1) + ':' + '<span style="background:antiquewhite; padding:5px; color:darkred;">' + elements[i].name + '</span ></h4>';
         infoHtml += elements[i].name;
         if (i < elements.length-1){
             infoHtml += ','
